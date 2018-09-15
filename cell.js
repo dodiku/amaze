004060c8cb4b2a7192ac1a3526216d7ac7be6456
dodiku <dayalon@dayalon-macbookpro.roam.corp.google.com>
2018-09-15 14:57:40 -0400

    getting the longest path

--- cell.js
+++ cell.js
@@ -13,72 +13,73 @@
 // https://en.wikipedia.org/wiki/Maze_generation_algorithm
 
 function Cell(i, j) {
-  this.i = i;
-  this.j = j;
-  this.walls = [true, true, true, true]; // top , right , bottom , left
-  this.visited = false;
-  this.morse = -1;
+  this.i = i
+  this.j = j
+  this.walls = [true, true, true, true] // top , right , bottom , left
+  this.visited = false
+  this.morse = -1
 
   this.checkNeighbors = function() {
-    var neighbors = [];
+    var neighbors = []
 
-    var top    = grid[index(i, j -1)];
-    var right  = grid[index(i+1, j)];
-    var bottom = grid[index(i, j+1)];
-    var left   = grid[index(i-1, j)];
+    var top = grid[index(i, j - 1)]
+    var right = grid[index(i + 1, j)]
+    var bottom = grid[index(i, j + 1)]
+    var left = grid[index(i - 1, j)]
 
     if (top && !top.visited) {
-      neighbors.push(top);
+      neighbors.push(top)
     }
     if (right && !right.visited) {
-      neighbors.push(right);
+      neighbors.push(right)
     }
     if (bottom && !bottom.visited) {
-      neighbors.push(bottom);
+      neighbors.push(bottom)
     }
     if (left && !left.visited) {
-      neighbors.push(left);
+      neighbors.push(left)
     }
 
     if (neighbors.length > 0) {
-      var r = floor(random(0, neighbors.length));
-      return neighbors[r];
+      var r = floor(random(0, neighbors.length))
+      return neighbors[r]
     } else {
-      return undefined;
+      return undefined
     }
-
-
   }
   this.highlight = function() {
-    var x = this.i*w;
-    var y = this.j*w;
-    noStroke();
-    fill(0, 0, 255, 100);
-    rect(x, y, w, w);
-
+    var x = this.i * w
+    var y = this.j * w
+    noStroke()
+    fill(0, 0, 255, 100)
+    rect(x, y, w, w)
   }
 
   this.show = function() {
-    var x = this.i*w;
-    var y = this.j*w;
-    stroke(0);
+    var x = this.i * w
+    var y = this.j * w
+    stroke(0)
     if (this.walls[0]) {
-      line(x    , y    , x + w, y);
+      line(x, y, x + w, y)
     }
     if (this.walls[1]) {
-      line(x + w, y    , x + w, y + w);
+      line(x + w, y, x + w, y + w)
     }
     if (this.walls[2]) {
-      line(x + w, y + w, x    , y + w);
+      line(x + w, y + w, x, y + w)
     }
     if (this.walls[3]) {
-      line(x    , y + w, x    , y);
+      line(x, y + w, x, y)
     }
 
-    if (this.visited) {
-      noStroke();
-      fill(255, 0, 255, 100);
-      rect(x, y, w, w);
+    if (this.morse != -1) {
+      noStroke()
+      fill(255, 0, 255, 50)
+      rect(x, y, w, w)
+    } else if (this.visited) {
+      noStroke()
+      fill(255, 0, 255, 100)
+      rect(x, y, w, w)
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -12,82 +12,95 @@
 // Recursive backtracker
 // https://en.wikipedia.org/wiki/Maze_generation_algorithm
 
+const letters = ['E', 'I']
+
 const numberOfRows = 8
 const canvasSize = 400 // in pixels
-const w = Math.floor(canvasSize/numberOfRows) - (canvasSize/100) // cell size
+const w = Math.floor(canvasSize / numberOfRows) - canvasSize / 100 // cell size
 
-let cols, rows;
-let current;
-let grid = [];
-let stack = [];
-let stackArchive = [];
+let cols, rows
+let current
+let grid = []
+let stack = []
+let stackArchive = []
 
 function setup() {
-  createCanvas(canvasSize, canvasSize);
-  cols = floor(width/w);
-  rows = floor(height/w);
+  createCanvas(canvasSize, canvasSize)
+  cols = floor(width / w)
+  rows = floor(height / w)
   // frameRate(5);
 
-  for (var   j = 0; j < rows; j++) {
+  for (var j = 0; j < rows; j++) {
     for (var i = 0; i < cols; i++) {
-      var cell = new Cell(i, j);
-      grid.push(cell);
+      var cell = new Cell(i, j)
+      grid.push(cell)
     }
   }
-  current = grid[0];
+  current = grid[0]
 }
 
 function draw() {
-  background(255);
+  background(255)
   for (var i = 0; i < grid.length; i++) {
-    grid[i].show();
+    grid[i].show()
   }
 
-  current.visited = true;
-  current.highlight();
+  current.visited = true
+  current.highlight()
   // STEP 1
-  var next = current.checkNeighbors();
-  
+  var next = current.checkNeighbors()
+
   if (next) {
-    next.visited = true;
+    next.visited = true
 
     // STEP 2
-    stack.push(current);
+    stack.push(current)
 
     // STEP 3
-    removeWalls(current, next);
+    removeWalls(current, next)
 
     // STEP 4
-    current = next;
+    current = next
   } else if (stack.length > 0) {
-    stackArchive = stackArchive.length < stack.length ? stack.slice() : stackArchive
+    stackArchive =
+      stackArchive.length < stack.length ? stack.slice() : stackArchive
     current = stack.pop()
+  } else {
+    // changing the color of the
+    highlightPath(stackArchive, letters)
   }
 }
 
 function index(i, j) {
-  if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
-    return -1;
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1
   }
-  return i + j * cols;
+  return i + j * cols
 }
 
-
 function removeWalls(a, b) {
-  var x = a.i - b.i;
+  var x = a.i - b.i
   if (x === 1) {
-    a.walls[3] = false;
-    b.walls[1] = false;
+    a.walls[3] = false
+    b.walls[1] = false
   } else if (x === -1) {
-    a.walls[1] = false;
-    b.walls[3] = false;
+    a.walls[1] = false
+    b.walls[3] = false
   }
-  var y = a.j - b.j;
+  var y = a.j - b.j
   if (y === 1) {
-    a.walls[0] = false;
-    b.walls[2] = false;
+    a.walls[0] = false
+    b.walls[2] = false
   } else if (y === -1) {
-    a.walls[2] = false;
-    b.walls[0] = false;
+    a.walls[2] = false
+    b.walls[0] = false
   }
+}
+
+const highlightPath = (highlightStach, letters) => {
+  grid.forEach(square => {
+    if (highlightStach.includes(square)) {
+      square.morse = letters[Math.floor(Math.random() * letters.length)]
+    }
+  })
 }
