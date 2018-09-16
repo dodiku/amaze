3f8c06cc647f2c984cf60fb34668aa1eeaaafe40
teh-f00l <vihavira@protonmail.com>
2018-09-15 20:12:15 -0400

    added hint function

--- scripts/game.js
+++ scripts/game.js
@@ -10,15 +10,84 @@ function keyTyped() {
       if (key.toLowerCase() === next.letter.toLowerCase()) {
         current = grid[index(next.i, next.j)]
         stackArchivePosition += 1
-        // window.setTimeout(showHint(), 3000);
+        window.setTimeout(showHint(next), 10000);
       }
     }
   }
 }
 
-// function showHint() {
-//     console.log('showHint');
-// }
+function showHint(x) {
+  var elt = document.getElementById('morse');
+  // var div1 = createDiv('this is the child');
+  // div1.id('moose');
+  // elt.child(div1)
+
+  elt.innerHTML = x.morse.toLowerCase();
+  for (let i in englishToMorse) {
+    if (i == x.morse.toLowerCase()) {
+      elt.innerHTML += englishToMorse[i];
+    }
+  }
+}
+
+// const morseToEnglish = {
+//   '.-': 'a',
+//   '-...': 'b',
+//   '-.-.': 'c',
+//   '-..': 'd',
+//   '.': 'e',
+//   '..-.': 'f',
+//   '--.': 'g',
+//   '....': 'h',
+//   '..': 'i',
+//   '.---': 'j',
+//   '-.-': 'k',
+//   '.-..': 'l',
+//   '--': 'm',
+//   '-.': 'n',
+//   '---': 'o',
+//   '.--.': 'p',
+//   '--.-': 'q',
+//   '.-.': 'r',
+//   '...': 's',
+//   '-': 't',
+//   '..-': 'u',
+//   'c': 'v',
+//   '.--': 'w',
+//   '-..-': 'x',
+//   '-.--': 'y',
+//   '--..': 'z'
+// };
+
+// const englishToMorse = {
+//   'a': '.-',
+//   'b': '-...',
+//   'c': '-.-.',
+//   'd': '-..',
+//   'e': '.',
+//   'f': '..-.',
+//   'g': '--.',
+//   'h': '....',
+//   'i': '..',
+//   'j': '.---',
+//   'k': '-.-',
+//   'l': '.-..',
+//   'm': '--',
+//   'n': '-.',
+//   'o': '---',
+//   'p': '.--.',
+//   'q': '--.-',
+//   'r': '.-.',
+//   's': '...',
+//   't': '-',
+//   'u': '..-',
+//   'v': '...-',
+//   'w': '.--',
+//   'x': '-..-',
+//   'y': '-.--',
+//   'z': '--..',
+// };
+
 //
 // function startIdleTimer() {
 //         // $(document).ready(function () {
